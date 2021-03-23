e482f32eb73cf813378116e1db468ba61f55d090
edgarherbar <edgar_david00@hotmail.com>
2021-03-22 22:38:21 -0600

    Todo funcionando, plantilla

--- Project/main.py
+++ Project/main.py
@@ -13,34 +13,18 @@ from flask_sqlalchemy import model
 from werkzeug.utils import redirect
 from . import db
 from . import models
+from flask_principal import Principal, Permission, RoleNeed
+
 main = Blueprint('main', __name__)
 
 
 @main.route('/')
 def index():
 
-    if current_user.has_role('admin'):
+    if RoleNeed('admin'):
         admin = True
-        return render_template('index.html', admin=admin, name=current_user.name)
-    if current_user.has_role('cliente'):
+        return render_template('index.html', admin=admin)
+    if RoleNeed('admin'):
         cliente = True
-        return render_template('index.html', cliente=cliente, name=current_user.name)
+        return render_template('index.html', cliente=cliente)
     return render_template('index.html')
-
-
-client = pymongo.MongoClient(
-    "mongodb+srv://admin:gmJR1NOhBmEEQm9t@cluster0.c8eub.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
-# test
-try:
-    db = client['ayuda_humanitaria']
-    # define collection
-    collection = db['sede']
-    if db:
-        print('✅ Coneccion establecida ' + str(collection))
-
-except Exception as e:
-    print('❌ Coneccion no establecida')
-    print(e)
-'''
-
-'''
