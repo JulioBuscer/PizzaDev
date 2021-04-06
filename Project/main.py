fd34a36d1253a51d49cd3db0a36298d0ab3e30fa
GwendoAru19 <gwendolinealcantar19@gmail.com>
2021-04-05 20:10:03 -0500

    crud proveedor

--- Project/main.py
+++ Project/main.py
@@ -13,7 +13,6 @@ from flask_sqlalchemy import model
 from werkzeug.utils import redirect
 from . import dbSQL, dbMongo
 from . import models
-from .models import Proveedor
 from flask_principal import Principal, Permission, RoleNeed
 
 main = Blueprint('main', __name__)
@@ -32,11 +31,13 @@ def index():
 
 @main.route("/usuario")
 def mostrarDatosUsuario():
-    return render_template("usuario.html")
+    usu = models.User.query.all()
+    return render_template("usuario.html",usuarios = usu)
 
 @main.route("/proveedores")
 def proveedores():
-    return render_template("proveedores.html")
+    pro = models.Proveedor.query.all()
+    return render_template("proveedores.html",proveedores = pro)
 
 @main.route('/registrarProveedor',methods=['GET'])
 def registrarProveedor():
@@ -44,7 +45,6 @@ def registrarProveedor():
 
 @main.route('/guardarProveedor',methods=['GET','POST'])
 def guardarProveedor():
-    
     if request.method=='POST':
         # print(request.form.get("txtEmpresa"))
         pro=models.Proveedor(
@@ -57,9 +57,77 @@ def guardarProveedor():
         dbSQL.session.commit()
     return render_template('proveedores.html')
 
-@main.route('/registrarUsuario',methods=['GET','POST'])
+@main.route("/modificarProveedor",methods=['POST','GET'])
+def modificarProveedor():
+    if request.method == 'GET':
+        id = request.args.get("id")
+        proveedor = dbSQL.session.query(models.Proveedor).filter(models.Proveedor.idProveedor == id).first()
+        print(proveedor.empresa)
+        return render_template("registrarProveedor.html", proveedor=proveedor)
+    if request.method == 'POST':
+        id = request.form.get("id")
+        pro = db.session.query(models.Proveedor).filter(models.Proveedor.idProveedor==id).first()
+        pro.empresa = request.form.get("txtEmpresa")
+        pro.direccionPro = request.form.get("txtDireccion")
+        pro.email = request.form.get("txtEmail")
+        pro.representante = request.form.get("txtTelefono")
+        pro.telefono = request.form.get("txtRepresentante")
+        dbSQL.session.add(pro)
+        dbSQL.session.commit()
+    return redirect(url_for('proveedores'))
+
+@main.route('/eliminarProveedor',methods=['GET','POST'])
+def eliminarProveedor():
+    if request.method == 'GET':
+        idProveedor = request.args.get("id")
+        pro =models.Proveedor.query.get(idProveedor)
+        dbSQL.session.delete(pro)
+        dbSQL.session.commit()
+    return redirect(url_for('proveedores'))
+
+@main.route('/registrarUsuario',methods=['GET'])
 def registrarUsuario():
     return render_template('registrarUsuario.html')
+
+@main.route('/guardarUsuario',methods=['GET','POST'])
+def guardarUsuario():
+    if request.method=='POST':
+        # print(request.form.get("txtEmpresa"))
+        use=models.User(
+            name=request.form.get("txtNombre"),
+            email=request.form.get("txtCorreo"),
+            password=request.form.get("txtPassword"),
+            roles=request.form.get("roles"))
+        dbSQL.session.add(use)
+        dbSQL.session.commit()
+    return render_template('usuario.html')
+
+@main.route('/eliminarUsuario',methods=['GET','POST'])
+def eliminarUsuario():
+    if request.method == 'GET':
+        id = request.args.get("id")
+        use =models.User.query.get(id)
+        dbSQL.session.delete(use)
+        dbSQL.session.commit()
+    return redirect(url_for('usuario'))
+
+@main.route("/modificarUsuario",methods=['POST','GET'])
+def modificarUsuario():
+    if request.method == 'GET':
+        id = request.args.get("id")
+        user = db.session.query(Alumnos).filter(Alumnos.id == id).first()
+        return render_template("modificar.html", alumno=alum)
+    if request.method == 'POST':
+        id = request.form.get("id")
+        alum = db.session.query(Alumnos).filter(Alumnos.id==id).first()
+        alum.nombre = request.form.get("txtNombre")
+        alum.apaterno = request.form.get("txtApaterno")
+        alum.amaterno = request.form.get("txtAmaterno")
+        alum.email = request.form.get("txtMail")
+        alum.email = request.form.get("txtPassword")
+        dbSQL.session.add(alum)
+        dbSQL.session.commit()
+    return redirect(url_for('mostrarDatos'))
 
 @main.route('/ventas')
 def ventas():
