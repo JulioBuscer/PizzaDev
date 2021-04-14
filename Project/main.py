8d8eb608770c20129fe831ee0395b6bab79fb130
Alma1906 <almacabrera1906@gmail.com>
2021-04-14 00:40:50 -0500

    14-04-21

--- Project/main.py
+++ Project/main.py
@@ -15,124 +15,360 @@ from . import dbSQL #dbMongo
 from . import models
 from . models import User
 from . models import Persona
+from . models import Direccion
+from . models import personas_direcciones
+from werkzeug.security import generate_password_hash
 from flask_principal import Principal, Permission, RoleNeed
+import shortuuid
+import hashlib
 
 main = Blueprint('main', __name__)
-
+arregloDir = []
+arreglo_ventas_per = []
+arregloDetalle = []
+arregloAll =[]
 
 @main.route('/perfil',methods=['GET','POST'])
 def perfil():
+        if current_user.has_role('cliente'):
+            arregloDir.clear()
+            #Inserción y actualización de datos 
+            cliente = True
+            per = Persona()
+            
+            id=current_user.id
+            user_=User.query.get(id)
+
+            user = dbSQL.session.query(models.Persona,models.User).join(models.User, models.Persona.idUsuario == models.User.id).filter(models.Persona.idUsuario==id) 
+            print(user)
+            apellidoP= ''
+            per=0
+            for d in user:
+                per=Persona.query.get(d.Persona.idPersona)
+                apellidoP=d.Persona.apellidoP
+            
+            if apellidoP == '':
+                persona= Persona(
+                    nombre=current_user.name,
+                    apellidoP = '-',
+                    apellidoM = '-',
+                    telefono = '-',
+                    fotografia = '-',
+                    idUsuario=current_user.id
+                )
+                print('print'+str(current_user.id))
+                dbSQL.session.add(persona)
+                dbSQL.session.commit()
+            
+            else:
+                user = dbSQL.session.query(models.Persona,models.User).join(models.User, models.Persona.idUsuario == models.User.id).filter(models.Persona.idUsuario==id)                 
+            query = 'select d.idDireccion,d.calle,d.colonia,d.numeroInt,d.numeroExt,d.codigoPostal,d.descripcion from personas_direcciones pd inner join persona p on pd.idPersona = p.idPersona inner join direccion d on pd.idDireccion = d.idDireccion inner join user u on p.idUsuario = u.id where u.id = '+ str(id) +' ;'
+                
+            direcciones= dbSQL.session.execute(query)
+            for x in direcciones:
+                arregloDir.append({
+                    "id":x.idDireccion,
+                    "calle": x.calle,
+                    "colonia": x.colonia,
+                    "numInt": x.numeroInt,
+                    "numExt": x.numeroExt,
+                    "cp": x.codigoPostal,
+                    "descripcion": x.descripcion
+                })
+            return render_template('perfil.html', cliente=cliente,name=current_user.name,user=user,user_=user_, direcciones=arregloDir)
         
+        if current_user.has_role('admin'):
+            arregloDir.clear()
+            #Inserción y actualización de datos 
+            admin = True
+            per = Persona()
+            
+            id=current_user.id
+            user_=User.query.get(id)
+            print('-------------'+str(id))
+            
+            user = dbSQL.session.query(models.Persona,models.User).join(models.User, models.Persona.idUsuario == models.User.id).filter(models.Persona.idUsuario==id) 
+            print(user)
+            apellidoP= ''
+            per=0
+            for d in user:
+                per=Persona.query.get(d.Persona.idPersona)
+                apellidoP=d.Persona.apellidoP
+                print ('--------------aksnkj'+str(apellidoP))
+            
+            if apellidoP == '':
+                persona= Persona(
+                    nombre=current_user.name,
+                    apellidoP = '',
+                    apellidoM = '',
+                    telefono = '',
+                    fotografia = '',
+                    idUsuario=current_user.id
+                )
+                dbSQL.session.add(persona)
+                dbSQL.session.commit()
+            
+            else:
+                user = dbSQL.session.query(models.Persona,models.User).join(models.User, models.Persona.idUsuario == models.User.id).filter(models.Persona.idUsuario==current_user.id)                 
+            query = 'select d.idDireccion,d.calle,d.colonia,d.numeroInt,d.numeroExt,d.codigoPostal,d.descripcion from personas_direcciones pd inner join persona p on pd.idPersona = p.idPersona inner join direccion d on pd.idDireccion = d.idDireccion inner join user u on p.idUsuario = u.id where u.id = '+ str(id) +' ;'
+                
+            direcciones= dbSQL.session.execute(query)
+            for x in direcciones:
+                arregloDir.append({
+                    "id":x.idDireccion,
+                    "calle": x.calle,
+                    "colonia": x.colonia,
+                    "numInt": x.numeroInt,
+                    "numExt": x.numeroExt,
+                    "cp": x.codigoPostal,
+                    "descripcion": x.descripcion
+                })
+            
+            return render_template('perfil.html', admin=admin,name=current_user.name,user=user,user_=user_, direcciones=arregloDir)
+        return redirect(url_for('main.index'))
+
+@main.route('/agregarDatos',methods=['GET','POST'])
+def agregarDatos():
+    
     if current_user.has_role('cliente'):
+        
         cliente = True
-        per = Persona()
         
         id=current_user.id
         user_=User.query.get(id)
         print('-------------'+str(id))
+        #per=0
         
         user = dbSQL.session.query(models.Persona,models.User).join(models.User, models.Persona.idUsuario == models.User.id).filter(models.Persona.idUsuario==id) 
-        apellidoP= ''
-        per=0
         for d in user:
             per=Persona.query.get(d.Persona.idPersona)
-            apellidoP=d.Persona.apellidoP
-            print ('--------------aksnkj'+str(apellidoP))
         
-        if apellidoP == '':
-            persona= Persona(
-                nombre=current_user.name,
-                apellidoP = '',
-                apellidoM = '',
-                telefono = '',
-                fotografia = '',
-                idUsuario=current_user.id
-            )
-            dbSQL.session.add(persona)
-            dbSQL.session.commit()
-        
-        if request.form.get("btnActualizarDatos"):
-            user_.name=request.form.get('txtNombre') 
-            user_.email=request.form.get('txtEmail')
-            #user_.password=request.form.get('txtContrasena')
-                            
-            per.nombre=request.form.get('txtNombre')
-            per.apellidoP=request.form.get('txtApellidoP')
-            per.apellidoM=request.form.get('txtApellidoM')
-            per.telefono=request.form.get('txtTelefono')
-            per.fotografia=request.form.get('txtFoto')
-            #dato.Persona.idUsuario=current_user.id
+        user_.name=request.form.get('txtNombre') 
+        user_.email=request.form.get('txtEmail')            
+
+        per.nombre=request.form.get('txtNombre')
+        per.apellidoP=request.form.get('txtApellidoP')
+        per.apellidoM=request.form.get('txtApellidoM')
+        per.telefono=request.form.get('txtTelefono')
+        per.fotografia=request.form.get('textarea')
+        #dato.Persona.idUsuario=current_user.id
             
-            dbSQL.session.add(user_)
-            dbSQL.session.add(per)
-            dbSQL.session.commit()
-            
-        user = dbSQL.session.query(models.Persona,models.User).join(models.User, models.Persona.idUsuario == models.User.id).filter(models.Persona.idUsuario==id) 
+        dbSQL.session.add(user_)
+        dbSQL.session.add(per)
+        dbSQL.session.commit()
         
-        return render_template('perfil.html', cliente=cliente,name=current_user.name,user=user,user_=user_)
+        arregloDir.clear()
+        
+        return redirect('/perfil')    
     
-    if current_user.has_role('admin'):  
+    if current_user.has_role('admin'):
+        
         admin = True
         
         id=current_user.id
         user_=User.query.get(id)
-        print(str('--------------')+str(id))  
+        print('-------------'+str(id))
+        #per=0
         
         user = dbSQL.session.query(models.Persona,models.User).join(models.User, models.Persona.idUsuario == models.User.id).filter(models.Persona.idUsuario==id) 
-        
-        apellidoP= ''
-        per=0
         for d in user:
             per=Persona.query.get(d.Persona.idPersona)
-            apellidoP=d.Persona.apellidoP
-            print ('--------------aksnkj'+str(apellidoP))
         
-        if apellidoP == '':
-            persona= Persona(
-                nombre=current_user.name,
-                apellidoP = '',
-                apellidoM = '',
-                telefono = '',
-                fotografia = '',
-                idUsuario=current_user.id
-            )
-            dbSQL.session.add(persona)
-            dbSQL.session.commit()
-        
-        if request.form.get("btnActualizarDatos"):
-            user_.name=request.form.get('txtNombre') 
-            user_.email=request.form.get('txtEmail')
-            #user_.password=request.form.get('txtContrasena')
+        user_.name=request.form.get('txtNombre') 
+        user_.email=request.form.get('txtEmail')           
                             
-            per.nombre=request.form.get('txtNombre')
-            per.apellidoP=request.form.get('txtApellidoP')
-            per.apellidoM=request.form.get('txtApellidoM')
-            per.telefono=request.form.get('txtTelefono')
-            per.fotografia=request.form.get('txtFoto')
-            #dato.Persona.idUsuario=current_user.id
+        per.nombre=request.form.get('txtNombre')
+        per.apellidoP=request.form.get('txtApellidoP')
+        per.apellidoM=request.form.get('txtApellidoM')
+        per.telefono=request.form.get('txtTelefono')
+        per.fotografia=request.form.get('textarea')
+        #dato.Persona.idUsuario=current_user.id
             
-            dbSQL.session.add(user_)
-            dbSQL.session.add(per)
-            dbSQL.session.commit()
+        dbSQL.session.add(user_)
+        dbSQL.session.add(per)
+        dbSQL.session.commit()
         
+        arregloDir.clear()
+        
+        return redirect('/perfil')    
+    return redirect(url_for('main.index'))
+    
+@main.route('/eliminarDireccion')
+def eliminarDireccion():
+    if current_user.has_role('admin'):
+        admin = True
+        #id=current_user.id
+        direccion = Direccion()
+        id = request.args.get("id")
+        direccion= Direccion.query.get(id)
+        dbSQL.session.delete(direccion)
+        dbSQL.session.commit()
+        
+        arregloDir.clear()
+        
+        return redirect('/perfil')
+        
+        #return render_template('perfil.html', admin=admin, name=current_user.name,direcciones=direcciones,user=user)
+    
+    if current_user.has_role('cliente'):
+        cliente = True
+        #id=current_user.id
+        
+        direccion = Direccion()
+        id = request.args.get("id")
+        direccion= Direccion.query.get(id)
+        dbSQL.session.delete(direccion)
+        dbSQL.session.commit()
+        
+        arregloDir.clear()
+        
+        return redirect('/perfil')
+    
+    return redirect(url_for('main.index'))
+
+@main.route('/agregarDirección',methods=['GET','POST'])
+def agregarDireccion():
+    if current_user.has_role('cliente'):
+        cliente = True
+        
+        id=current_user.id
         user = dbSQL.session.query(models.Persona,models.User).join(models.User, models.Persona.idUsuario == models.User.id).filter(models.Persona.idUsuario==id) 
-        return render_template('perfil.html', admin=admin,name=current_user.name,user=user,user_=user_)
+        for d in user:
+            per=Persona.query.get(d.Persona.idPersona)
+        
+        insercion= False
+        dir= Direccion(
+            calle=request.form.get('txtCalle'),
+            colonia = request.form.get('txtColonia'),
+            numeroInt = request.form.get('txtNumInt'),
+            numeroExt = request.form.get('txtNumExt'),
+            codigoPostal = request.form.get('txtCP'),
+            descripcion = request.form.get('txtDescripcion'))
+        dbSQL.session.add(dir)
+        dbSQL.session.commit()
+            
+        insercion= True
+        if insercion:
+            lastid = dbSQL.session.query(Direccion).order_by(Direccion.idDireccion.desc()).first()
+            per_dir= personas_direcciones.insert().values(idPersona=per.idPersona,idDireccion=(lastid.idDireccion))
+            dbSQL.session.execute(per_dir)
+            dbSQL.session.commit()
+            
+            arregloDir.clear()
+        
+            return redirect('/perfil')
+    
+    if current_user.has_role('admin'):
+        admin = True
+        
+        id=current_user.id
+        user = dbSQL.session.query(models.Persona,models.User).join(models.User, models.Persona.idUsuario == models.User.id).filter(models.Persona.idUsuario==id) 
+        for d in user:
+            per=Persona.query.get(d.Persona.idPersona)
+        
+        insercion= False
+        dir= Direccion(
+            calle=request.form.get('txtCalle'),
+            colonia = request.form.get('txtColonia'),
+            numeroInt = request.form.get('txtNumInt'),
+            numeroExt = request.form.get('txtNumExt'),
+            codigoPostal = request.form.get('txtCP'),
+            descripcion = request.form.get('txtDescripcion'))
+        dbSQL.session.add(dir)
+        dbSQL.session.commit()
+            
+        insercion= True
+        if insercion:
+            lastid = dbSQL.session.query(Direccion).order_by(Direccion.idDireccion.desc()).first()
+            per_dir= personas_direcciones.insert().values(idPersona=per.idPersona,idDireccion=(lastid.idDireccion))
+            dbSQL.session.execute(per_dir)
+            dbSQL.session.commit()
+            
+            arregloDir.clear()
+
+            return redirect('/perfil')
     return redirect(url_for('main.index'))
 
-@main.route('/pedidosDia')
-def pedidosDia():
+@main.route('/modificarDirección')
+def modificarDireccion():
+    if current_user.has_role('admin'):
+        admin = True
+        
+        #per=Persona.query.get(d.Persona.idPersona)
+        id = request.args.get("id")
+        print(request.args.get("colonia"))
+        dir=dbSQL.session.query(Direccion).filter(Direccion.idDireccion==id).first()
+        
+        dir.calle =request.args.get("calle")
+        dir.colonia =request.args.get("colonia")
+        dir.numeroInt =request.args.get("numeroInt")
+        dir.numeroExt =request.args.get("numeroExt")
+        dir.codigoPostal = request.args.get("cp")
+        dir.descripcion = request.args.get("descripcion")
+            
+        dbSQL.session.add(dir)
+        dbSQL.session.commit()
+        
+        arregloDir.clear()
+        
+        return redirect('/perfil')
+        
+    
     if current_user.has_role('cliente'):
         cliente = True
-        return render_template('pedidosDia.html', cliente=cliente,name=current_user.name)
+        
+        #per=Persona.query.get(d.Persona.idPersona)
+        id = request.args.get("id")
+        print(request.args.get("colonia"))
+        dir=dbSQL.session.query(Direccion).filter(Direccion.idDireccion==id).first()
+        
+        dir.calle =request.args.get("calle")
+        dir.colonia =request.args.get("colonia")
+        dir.numeroInt =request.args.get("numeroInt")
+        dir.numeroExt =request.args.get("numeroExt")
+        dir.codigoPostal = request.args.get("cp")
+        dir.descripcion = request.args.get("descripcion")
+            
+        dbSQL.session.add(dir)
+        dbSQL.session.commit()
+        
+        arregloDir.clear()
+        
+        return redirect('/perfil')
+        
     return redirect(url_for('main.index'))
 
-@main.route('/pedidosSemana')
-def pedidosSemana():
+@main.route('/pedidos',methods=['GET','POST'])
+def pedidos():
     if current_user.has_role('cliente'):
         cliente = True
-        return render_template('pedidosSemana.html', cliente=cliente,name=current_user.name)
+        
+        arreglo_ventas_per.clear()
+        arregloDetalle.clear()
+        
+        id=current_user.id
+        user = dbSQL.session.query(models.Persona,models.User).join(models.User, models.Persona.idUsuario == models.User.id).filter(models.Persona.idUsuario==id) 
+        for d in user:
+            per=Persona.query.get(d.Persona.idPersona)
+            idPersona = d.Persona.idPersona
+        
+        query = 'select r.nombre, v.cantidad, v.subtotal, r.costo, v.fecha,v.idVenta from venta_recetario vr inner join venta v on vr.idVenta = v.idVenta inner join recetario r on vr.idRecetario = r.idRecetario where v.idPersona = '+ str(idPersona) +';'       
+        ventas_persona= dbSQL.session.execute(query)
+              
+        for x in ventas_persona:
+            
+            arreglo_ventas_per.append({
+                "id":str(shortuuid.uuid()),
+                "fecha": x.fecha,
+                "sabor":x.nombre,
+                "cantidad":x.cantidad,
+                "costo":x.costo,
+                "subtotal":x.subtotal
+            })
+            
+            print ('ventas'+str(arreglo_ventas_per))  
+        return render_template('pedidos.html', cliente=cliente,name=current_user.name,arreglo_ventas_per=arreglo_ventas_per)
     return redirect(url_for('main.index'))
-
     
 @main.route('/')
 def index():
